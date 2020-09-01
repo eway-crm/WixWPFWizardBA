97346528729a8e00c818f503a5264d98ae1efc79
rstefko <roman.stefko@eway-crm.com>
2020-09-01 10:48:32 +0200

    Display label also during update.

---
+++ WixWPFWizardBA/Converters/InvertedBooleanToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WixWPFWizardBA.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class InvertedBooleanToVisibilityConverter : IValueConverter
+    {
+        private BooleanToVisibilityConverter converter = new BooleanToVisibilityConverter();
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var result = this.converter.Convert(value, targetType, parameter, culture) as Visibility?;
+
+            return result == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var result = this.ConvertBack(value, targetType, parameter, culture) as bool?;
+
+            return !result;
+        }
+    }
+}
